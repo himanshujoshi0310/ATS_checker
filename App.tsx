ae33f84f028f0b861291f032418a5435f5e959f5
fmtvp <93467678+fmtvp@users.noreply.github.com>
2026-01-18 18:59:04 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -261,7 +261,7 @@ const App: React.FC = () => {
 
       <footer className="py-8 mt-auto border-t bg-white">
         <div className="max-w-7xl mx-auto px-4 text-center">
-          <p className="text-slate-400 text-[10px] font-bold uppercase tracking-widest">© 2026 Enterprise Talent Systems Deployment</p>
+          <p className="text-slate-400 text-[10px] font-bold uppercase tracking-widest">© 2026 3Dumb Developers All Right Reserved</p>
         </div>
       </footer>
       <Analytics />
